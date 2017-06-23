331b5dbeba332c4ab236bb3dca7cb12829d86803
Joseph Rex <joerexko@gmail.com>
2017-06-22 19:27:00 -0500

    Remove unwanted files from old theme and add build process for static assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,38 @@
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
-const del = require('del');
-const parallel = require('concurrent-transform');
-const os = require('os');
 
 const $ = gulpLoadPlugins();
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
-const stream = browserSync.stream;
 
 const paths = {
   source: {
     scripts: [
-      'vendor/assets/components/smooth-scroll/smooth-scroll.js',
-      'src/js/**/*.js'
+      'static/js/vendor/barba.min.js',
+      'static/js/vendor/web-animations.min.js',
+      'static/js/vendor/prism.js',
+      'static/js/src/main.js',
     ],
     styles: [
-      'vendor/assets/components/aos/dist/aos.css',
-      'src/scss/**/*.scss'
-    ],
-    images: [
-      'src/images/**/*.*'
-    ],
-    templates: ['*.html']
+      'static/css/vendor/*.css',
+      'static/scss/**/*.scss'
+    ]
   },
   target: {
-    all:     'dist/**/*',
-    scripts: 'dist/js',
-    styles: 'dist/css',
-    images: 'dist/images',
+    scripts: 'static/js',
+    styles: 'static/css',
     sourcemaps: './maps',
   }
 };
-
-gulp.task('clean', () => del.sync([paths.target.all]) );
 
 gulp.task('styles', () => {
   gulp.src(paths.source.styles)
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
-    .pipe($.concat('bundle.css'))
+    .pipe($.concat('app.css'))
     .pipe($.autoprefixer({browsers: ['last 2 versions', '> 1%', 'Firefox ESR']}))
     .pipe($.cssnano())
     .pipe($.sourcemaps.write(paths.target.sourcemaps))
     .pipe(gulp.dest(paths.target.styles))
-    .pipe(stream());
 });
 
 gulp.task('scripts', () => {
@@ -57,52 +44,6 @@ gulp.task('scripts', () => {
     .pipe($.uglify())
     .pipe($.sourcemaps.write(paths.target.sourcemaps))
     .pipe(gulp.dest(paths.target.scripts))
-    .pipe(stream());
 });
 
-gulp.task('images', () => {
-  gulp.src(paths.source.images)
-    .pipe(gulp.dest(paths.target.images));
-});
-
-gulp.task('optimizedImages', () => {
-  gulp.src(paths.source.images)
-    // .pipe(parallel(
-    //   $.imageResize({ width: 600 }),
-    //   os.cpus().length
-    // ))
-    .pipe($.cache(
-      $.imagemin({
-        optimizationLevel: 3,
-        progressive: true,
-        interlaced: true,
-        // Keep IDs in SVG
-        svgoPlugins: [{cleanupIDs: false}]
-      })
-    ))
-    .pipe(gulp.dest(paths.target.images));
-});
-
-gulp.task('serve', ['styles', 'scripts'], () => {
-  browserSync.init({
-    server: {
-      baseDir: ".",
-      routes: {
-        '/styleguide': 'dist/styleguides.html'
-      }
-    }
-  });
-
-  gulp.watch(paths.source.styles, ['styles']);
-  gulp.watch(paths.source.scripts, ['scripts']);
-  gulp.watch(paths.source.templates).on('change', reload);
-});
-
-gulp.task('build', ['clean', 'scripts', 'styles', 'images'], () => {
-  return gulp.src(paths.target.all).pipe($.size({
-    title: 'build',
-    pretty: true
-  }))
-});
-
-gulp.task('default', ['scripts', 'styles', 'images', 'serve']);
+gulp.task('default', ['scripts', 'styles']);
